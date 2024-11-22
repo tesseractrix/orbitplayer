833dd3b57e1bf8ee26428f9cd3c87c3058bc9b02
tesseractrix <voxels@protonmail.com>
2024-11-22 14:00:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ document.addEventListener('contextmenu', function (e) {
 });
 
 // google ads
-async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1939121894357762"
-     crossorigin="anonymous">;
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1939121894357762"
+     crossorigin="anonymous"></script>
 
 var ul = document.getElementById("results");
 
