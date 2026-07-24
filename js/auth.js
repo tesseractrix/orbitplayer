e5bfdfff54677b2b59876529a242129d615191e1
tesseractrix <voxels@protonmail.com>
2026-07-24 13:20:10 -0300

    Add files via upload

---
+++ js/auth.js
@@ -0,0 +1,15 @@
+import { auth } from "./firebase-config.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+
+        window.location.href = "start.html";
+
+    }
+
+});
---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArSoUN8xIX7Cm-I4oQooyxK0diNjx2RyE",
+  authDomain: "orbit-player-2b105.firebaseapp.com",
+  projectId: "orbit-player-2b105",
+  storageBucket: "orbit-player-2b105.firebasestorage.app",
+  messagingSenderId: "1096650827222",
+  appId: "1:1096650827222:web:5f812ce1961973d5c47077"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
